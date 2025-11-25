e0aebfd3d0787493633dea20392d63ae5ab7c2bf
Diego Vilhalva <diegorocha450@yahoo.com>
2025-11-25 11:55:04 -0300

    create latest listing section and components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter } from "react-router-dom"
 import { ClerkProvider } from '@clerk/clerk-react'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { store } from './app/store.js'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -12,8 +14,11 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance={{variables:{colorPrimary:'#4f46e5'}}}>
-      <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance={{ variables: { colorPrimary: '#4f46e5' } }}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+
     </ClerkProvider>
   </BrowserRouter>,
 )
